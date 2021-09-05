13474d2bddbc825a52dc7c49db724a64a22ad10e
Salvatore Calderaro <46534450+Calder10@users.noreply.github.com>
2021-09-05 16:55:27 +0200

    add integrity of messages

--- AES.py
+++ AES.py
@@ -9,19 +9,20 @@ def apply_sha256(key):
 def encrypt(key,msg):
     cipher = AES.new(key, AES.MODE_EAX)
     nonce = cipher.nonce
+    print("Nonce",nonce)
     ciphertext, tag = cipher.encrypt_and_digest(msg.encode('utf-8'))
     return nonce, ciphertext, tag
 
 def decrypt(nonce, ciphertext, tag,key):
+    print("Nonce",nonce)
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(ciphertext)
     try:
         cipher.verify(tag)
+        print("The message is authentic")
         return plaintext.decode('utf-8')
     except:
+        print("Key incorrect or message corrupted")
         return False
-"""
-print("Shared Key",hex(K))
-"""
 
 
